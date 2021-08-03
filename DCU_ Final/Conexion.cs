18ca2ba4811bf0ec537cc001a42eb2469dd1ffe9
Caterina2001 <84465451+Caterina2001@users.noreply.github.com>
2021-08-03 01:17:31 -0400

    Add files via upload
    Aplicacion de escritorio de reconocimiento facial, posiblemnte usado en el ambito de salud.

---
+++ DCU_ Final/Conexion.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace DCU__Final
+{
+    public class Conexion
+    {
+        private static OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=DBDCU.accdb;");
+
+        public static string[] Nombre;
+        public static string[] Cedula;
+        public static string[] TipoSangre;
+        public static string[] Antecedentes;
+        public static string[] Contacto;
+        public static string[] Direccion;
+        private static byte[] Face;
+        public static List<byte[]> Listado = new List<byte[]>();
+
+        public static int TotalFace;
+
+        
+
+        public static bool GuardarImagen(string Nombre, string Cedula, string TipoSangre, string Antecedentes, string Contacto, string Direccion, byte[] Imagen)
+        {
+            conexion.Close();
+            conexion.Open();
+            OleDbCommand cmd = new OleDbCommand("INSERT INTO TablaDCU (Nombre,Cedula,TipoSangre,Antecedentes,Contacto,Direccion,Imagen) Values('" + Nombre + "', '" + Cedula + "', '" + TipoSangre + "', '" + Antecedentes + "', '" + Contacto + "', '" + Direccion + "', ?);", conexion);
+            OleDbParameter parImagen = new OleDbParameter("@Imagen", OleDbType.VarBinary, Imagen.Length);
+            parImagen.Value = Imagen;
+            cmd.Parameters.Add(parImagen);
+            int Resultado = cmd.ExecuteNonQuery();
+            conexion.Close();
+
+            return Convert.ToBoolean(Resultado);
+
+        }
+
+
+        public static DataTable Consultar(DataGridView DATA)
+        {
+            conexion.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DATA.DataSource = dt;
+            int Cont = dt.Rows.Count;
+            Nombre = new string[Cont];
+            Cedula = new string[Cont];
+            TipoSangre = new string[Cont];
+            Antecedentes = new string[Cont];
+            Contacto = new string[Cont];
+            Direccion = new string[Cont];
+            conexion.Close();
+            for (int i = 0; i < Cont; i++)
+            {
+                Nombre[i] = dt.Rows[i]["Nombre"].ToString();
+                Cedula[i] = dt.Rows[i]["Cedula"].ToString();
+                TipoSangre[i] = dt.Rows[i]["TipoSangre"].ToString();
+                Antecedentes[i] = dt.Rows[i]["Antecedentes"].ToString();
+                Contacto[i] = dt.Rows[i]["Contacto"].ToString();
+                Direccion[i] = dt.Rows[i]["Direccion"].ToString();
+                Face = (byte[])dt.Rows[i]["Imagen"];
+                Listado.Add(Face);
+
+            }
+
+
+            try
+            {
+                DATA.Columns[0].Width = 60;
+                DATA.Columns[1].Width = 160;
+                DATA.Columns[2].Width = 160;
+                DATA.Columns[3].Width = 160;
+                DATA.Columns[4].Width = 160;
+                DATA.Columns[5].Width = 160;
+                DATA.Columns[6].Width = 160;
+
+                for (int i = 0; i < Cont; i++)
+                {
+
+                    DATA.Rows[i].Height = 110;
+                }
+            }
+            catch
+            {
+
+            }
+
+            TotalFace = Cont;
+
+            return dt;
+
+        }
+
+
+        ////
+
+        public static byte[] ConvertImgToBinary(Image img)
+        {
+            Bitmap bmp = new Bitmap(img);
+            MemoryStream Memoria = new MemoryStream();
+            bmp.Save(Memoria, ImageFormat.Bmp);
+
+            byte[] imagen = Memoria.ToArray();
+
+            return imagen;/// arreglo de Binario de la imagen
+
+        }
+
+        public static Image ConvertBinaryToImg(int C)
+        {
+            Image Imagen;
+            byte[] img = Listado[C];
+            MemoryStream Memoria = new MemoryStream(img);
+            Imagen = Image.FromStream(Memoria);
+            Memoria.Close();
+            return Imagen;
+        }
+
+    }
+}
---
+++ DCU_ Final/Detector.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace DCU__Final
+{
+    partial class Detector
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.lblNum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(59, 57);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(771, 397);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(389, 489);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(89, 41);
+            this.btnRegistrar.TabIndex = 3;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(56, 475);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(107, 13);
+            this.lblCantidad.TabIndex = 4;
+            this.lblCantidad.Text = "Personas detectadas";
+            this.lblCantidad.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(169, 457);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(0, 13);
+            this.lblValor.TabIndex = 5;
+            this.lblValor.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(169, 475);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(13, 13);
+            this.lblNum.TabIndex = 6;
+            this.lblNum.Text = "0";
+            // 
+            // Detector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DCU__Final.Properties.Resources._25042016_fachada_emergencia_hospital_11272862_201902161300091;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.imageBox1);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.Name = "Detector";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "conf";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Label lblNum;
+    }
+}
+
